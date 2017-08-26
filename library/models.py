523f71f4c0df03f66922a7d5f461d2cf5da0fdff
S-eddin <elahmer2010@yahoo.com>
2017-08-26 11:50:24 +0200

    Update models.py

--- library/models.py
+++ library/models.py
@@ -10,13 +10,15 @@ class Genre(models.Model):
     """
     Model representing a book genre (e.g. Science Fiction, Non Fiction).
     """
-    Address = models.CharField(max_length=200, help_text="Enter a book genre (e.g. Science Fiction, French Poetry etc.)")
+    name = models.CharField(max_length=200,verbose_name = Address,
+
+ help_text="Enter a book genre (e.g. Science Fiction, French Poetry etc.)")
     
     def __str__(self):
         """
         String for representing the Model object (in Admin site etc.)
         """
-        return self. Address
+        return self.name
 
 from django.urls import reverse #Used to generate URLs by reversing the URL patterns
 
